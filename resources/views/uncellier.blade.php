e2d063867590af87b0c961605b67ce1c8b1517aa
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2023-01-25 15:32:34 -0500

    ajout css et class

--- resources/views/uncellier.blade.php
+++ resources/views/uncellier.blade.php
@@ -1,41 +1,44 @@
 <x-app-layout>
-    <h1><b>nom du cellier:</b> {{ $cellier->nom }}</h1>
+
+<section class="containerCatalogue">
     @forelse ($bouteilles as $bouteille)
+    <div class="carteCatalogue">
+        <h1><b>nom du cellier:</b> {{ $cellier->nom }}</h1>
+
         <div class="bouteille">
             <h1><b>id de la bouteille:</b> {{ $bouteille->id }}</h1>
-            @if (substr($bouteille->image, 0, 8) == 'https://')
-                <img src="{{ $bouteille->image }}" alt="{{ $bouteille->nom }}" width="100px">
-            @else
-                <img src="{{ asset('images/' . $bouteille->image) }}" alt="{{ $bouteille->nom }}" width="100px">
-            @endif
+            <img src="{{ $bouteille->image }}" alt="{{ $bouteille->nom }}">
             <h2><b>nom de la bouteille:</b> {{ $bouteille->nom }}</h2>
             <h3><b>quantite:</b> {{ $bouteille->quantite }}</h3>
             {{-- <a href="{{ route('bouteilles.show', $bouteille->id) }}">Les détails de la bouteille</a> --}}
+
         </div>
-        <form action="{{ route('bouteilles.drink', [$cellier->id, $bouteille->id]) }}" method="post">
-            @csrf
-            <button type="submit" class="btn btn-danger">Boire</button>
-        </form>
+        <div class="options">
+            <form action="{{ route('bouteilles.drink', [$cellier->id, $bouteille->id]) }}" method="post">
+                @csrf
+                <button type="submit" class="btn btn-danger">Boire</button>
+            </form>
 
-        <form action="{{ route('bouteilles.add', [$cellier->id, $bouteille->id]) }}" method="post">
-            @csrf
-            <button type="submit" class="btn btn-primary">Ajouter</button>
-        </form>
+            <form action="{{ route('bouteilles.add', [$cellier->id, $bouteille->id]) }}" method="post">
+                @csrf
+                <button type="submit" class="btn btn-primary">Ajouter</button>
+            </form>
 
-        <form action="{{ route('bouteilles.remove', [$cellier->id, $bouteille->id]) }}" method="post">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">Enlever</button>
-        </form>
-
-        <a href="{{ route('bouteilles.edit', [$cellier->id, $bouteille->id]) }}">
-            <button class="btn btn-primary">Modifier la bouteille</button>
-        </a>
+            <form action="{{ route('bouteilles.remove', [$cellier->id, $bouteille->id]) }}" method="post">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Enlever</button>
+            </form>
+            <a href="{{ route('bouteilles.edit', [$cellier->id, $bouteille->id]) }}">
+                    <button class="btn btn-primary">Modifier la bouteille</button>
+                </a>
+        </div>
+    </div>
     @empty
-        <p>Aucune bouteille disponible</p>
-        {{-- <a href="">Cliquez ici pour ajouter une bouteille</a> --}}
+    <p>Aucune bouteille disponible</p>
     @endforelse
+</section>
 
-    <a href="{{ route('celliers') }}">Retour à la liste des celliers</a>
-    <a href="{{ route('bouteilles.create', $cellier->id) }}">Ajouter une bouteille de vin au cellier</a>
+<a href="{{ route('celliers') }}">Retour à la liste des celliers</a>
+<a href="{{ route('bouteilles.create', $cellier->id) }}">Ajouter une bouteille de vin au cellier</a>
 </x-app-layout>
