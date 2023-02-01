a7ce602627fcfc2bc6a02afef71e72c245646eac
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2023-02-01 16:01:35 -0500

    ajout data-js-form pour validation formulaire

---
+++ resources/views/guest/cellier/editcellier.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <section class="form">
+        <h5 class="form-title">Editer un cellier</h5><br>
+        <form action="{{ route('celliers.update', $cellier->id) }}" method="post" data-js-form>
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="nom">Nom du cellier</label>
+                <input type="text" name="nom" id="nom" placeholder="{{old('nom', $cellier->nom)}}">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Appliquer</button>
+        </form>
+    </section>
+</x-app-layout>
