9c97ca326f9707c2ba921d729c31eaddd9a0ae4a
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2023-02-01 15:46:45 -0500

    ajout italique titre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,65 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
+        <script differ src="https://kit.fontawesome.com/918ba06b86.js" crossorigin="anonymous"></script>
+        <title>Vino Bueno</title>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSS-->
-    <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
-    <title>Vino Bueno</title>
-
-</head>
-
-<body>
-    <div class="accueilContent">
-        {{-- le logo ici --}}
-        {{-- le reste du menu ici comme celliers/creer un cellier/ profil ... etc--}}
-        {{-- @if (Route::has('login'))
-                <div>
-                    @auth
-                        <a href="{{ url('/celliers') }}">page des celliers</a>
-        @else
-        <a href="{{ route('login') }}">Connexion</a>
-
-        @if (Route::has('register'))
-        <a href="{{ route('register') }}">Inscription</a>
-        @endif
-        @endauth
-    </div>
-    @endif --}}
-
-    {{-- @auth--}}
-    <div class="accueilContent">
-        {{-- <div class="accueilTitle">
-                    vous etes a la page d'accueil
-                    <a href="{{ url('/celliers') }}">Voir vos celliers</a>
-    </div>
-    </div>
-    @else
-    <div>
-        <div>
-
-        </div>
-    </div>--}}
-    <div class="landingPage">
-
-        <div>
+    </head>
+    <body class="bodyLanding">
+        <div class="landingPage">
             <div class="titreLanding">
-                <h1><i>Vino</i></h1>
-                <h3>Jamais à sec grace à mon cellier connecté!</h3>
+                <h1><i> Vino <span class="bueno">Bueno</span></i></h1>
+                <h3><span class="mcc">Jamais à sec</span> grâce à mon cellier connecté!</h3>
             </div>
             <div class="liens">
-            <a href="{{ route('login') }}">Se connecter</a>
-            <a href="{{ route('register') }}">Creer un compte</a>
+                <a href="{{ route('login') }}">Se connecter</a>
+                <a href="{{ route('register') }}">Créer un compte</a>
             </div>
         </div>
-        {{-- @endauth--}}
-
-    </div>
-
-
-    {{-- footer ici si tu veux --}}
-    </div>
-</body>
-
+    </body>
 </html>
